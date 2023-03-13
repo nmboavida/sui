3401bcd26d66fa7a21bceba5b6a35064bca2b0f7
patrick <patrickkuo@me.com>
2023-03-13 23:26:23 +0000

    fix tests

--- crates/sui-cluster-test/src/test_case/native_transfer_test.rs
+++ crates/sui-cluster-test/src/test_case/native_transfer_test.rs
@@ -45,7 +45,7 @@ impl TestCaseImpl for NativeTransferTest {
             recipient_addr
         ];
         let data = ctx
-            .build_transaction_remotely("sui_transferObject", params)
+            .build_transaction_remotely("unsafe_transferObject", params)
             .await?;
         let mut response = ctx.sign_and_execute(data, "coin transfer").await;
 
@@ -63,7 +63,7 @@ impl TestCaseImpl for NativeTransferTest {
         let obj_to_transfer = *sui_objs.swap_remove(0).id();
         let params = rpc_params![signer, obj_to_transfer, 5000, recipient_addr, None::<u64>];
         let data = ctx
-            .build_transaction_remotely("sui_transferSui", params)
+            .build_transaction_remotely("unsafe_transferSui", params)
             .await?;
         let mut response = ctx.sign_and_execute(data, "coin transfer").await;
 
